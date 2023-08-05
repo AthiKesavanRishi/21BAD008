42d4a82a8f6df1da07d73500f8387a31954c5b5e
reactJS <athikesavan.21ad@kct.ac.in>
2023-08-05 14:33:29 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div>
+        
+    
+  
+    <h4><b>Strength</b></h4>
+    <ul>
+        <li>Punctual</li>
+        <li>Hard Work</li>
+    </ul>
+    <h4><b>Weakness</b></h4>
+    <ul>
+        <li>Public Speaking</li>
+        <li>Fear</li>
+    </ul>
+    <h4><b>Education</b></h4>
+    <ol type="1">
+        <li>Done my Schooling at Christ The King</li>
+        <li>Currently Pursuing Artificial Intelligence and Data Science at Kumaraguru</li>
+    </ol>
+    <h4><b>Skills</b></h4>
+    <ol type="A">
+        <li>Python</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Machine Learning</li>
+        <li>Data Science</li>
+    </ol>
+    <h4><b>Contact Me</b></h4>
+    <ol>
+        <li><a href="https://www.linkedin.com/in/athi-kesavan-rishi-a93301237">linkedin</a></li>
+        <li><a href="https://www.instagram.com/?utm_source=pwa_homescreen&__pwa=1">instagram</a></li>
+    </ol>
+
+
+    <div class = "grid-container" >
+        <div class = "grid-item">ML Engineer</div>
+        <div class = "grid-item">Data Scientist</div>
+
+
+    
+    <div class = "flex-container" >
+        <div class = "flex-item">Full Stack</div>
+        <div class = "flex-item">Web Developer</div>
     </div>
+    </div>
+</div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,45 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import image from './me.jpg'
+
+class StateManagement extends React.Component {
+  constructor () {
+    super()
+    this.state = {year : 'Third'}
+  }
+  
+  first = () => this.setState({year : 'First'})
+  second= () => this.setState({year : 'Second'})
+  third = () => this.setState({year : 'Third'})
+  fourth = () => this.setState({year : 'Fourth'})
+
+  render () {
+    return (
+      <div>
+        <h1><b>ATHI KESAVAN</b></h1>
+        <div>
+          <img src={image} alt='image'></img>
+        </div>
+
+        <h2 id="super"><u>ARTIFICIAL INTELLIGENCE AND DATA SCIENCE</u></h2>
+        <h3><i>I am a <span class = "ds">Smart Boy</span></i></h3>
+        <h1>I am studying {this.state.year} Year</h1>
+        <button type='button' onClick={this.first}>First</button>
+        <button type='button' onClick={this.second}>Second</button>
+        <button type='button' onClick={this.third}>Third</button>
+        <button type='button' onClick={this.fourth}>Fourth</button>
+      </div>
+    );  
+  }
+} 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <StateManagement/>
     <App />
+    
   </React.StrictMode>
 );
 
